e9c82fc54e8ee308eda2e9cb690b4bf21baf95eb
Dawid Bator <dawid.bathor@gmail.com>
2018-04-28 23:50:30 +0200

    pliki javascript, potrzebne do responsywnego slidera, waypoints, animacje

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    /*telefon herobox*/
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated tada');
+    }, {
+        offset: '50%'
+    });
+    
+    /*telefon kontakt*/
+        $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated bounce');
+    }, {
+        offset: '100%'
+    });
+    
+    /*facebook*/
+            $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated tada');
+    }, {
+        offset: '100%'
+    });
+       
+
+    });
+    
+    
+    
